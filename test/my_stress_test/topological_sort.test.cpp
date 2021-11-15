d63898679836a9f6d1a99a36720f82f79b808d00
atree <atree.public@gmail.com>
2021-11-15 23:28:19 +0900

    fix test/my_stress_test/topological_sort.test.cpp

--- test/my_stress_test/topological_sort.test.cpp
+++ test/my_stress_test/topological_sort.test.cpp
@@ -1,11 +1,11 @@
 #define PROBLEM "https://judge.u-aizu.ac.jp/onlinejudge/description.jsp?id=ITP1_1_A"
 
-#include "lib/graph/topological_sort.hpp"
 #include <algorithm>
 #include <cassert>
 #include <iostream>
 #include <numeric>
 #include <random>
+#include "lib/graph/topological_sort.hpp"
 using namespace std;
 
 random_device seed_gen;
@@ -17,17 +17,17 @@ void test() {
     for (size_t _ = 0; _ < 500000; _++) {
         size_t n = rand() % 20, m = n ? rand() % 30 : 0;
         bool is_cycle = rand() % 2;
-        vector<int> ordered(n);
+        vector<size_t> ordered(n);
         iota(begin(ordered), end(ordered), 0);
         shuffle(begin(ordered), end(ordered), rnd);
-        vector<vector<int>> ed(n);
+        vector<vector<size_t>> ed(n);
         for (size_t i = 0; i < m; i++) {
             int a = rand() % n;
             int b = rand() % n;
             if (is_cycle and a >= b) continue;
             ed[ordered[a]].push_back(ordered[b]);
         }
-        vector<int> ret = topological_sort(ed);
+        const auto ret = topological_sort(ed);
         if (is_cycle) assert(size(ret) == n);
         else
             assert(size(ret) <= n);
